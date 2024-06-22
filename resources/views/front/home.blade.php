318b66d5c63afc20de942ebcc55419c40ebf19ce
wicho1965 <lualroru65>
2024-06-22 11:56:04 -0400

    22/06/2024 11:55

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,6 +3,15 @@
 @section('titulo', 'Home')
 
 @section('content')
-    desde el home
+    desde el home <<button type="button" class="btn btn-outline-primary">Primary</button>
+    <button type="button" class="btn btn-outline-secondary">Secondary</button>
+    <button type="button" class="btn btn-outline-success">Success</button>
+    <button type="button" class="btn btn-outline-danger">Danger</button>
+    <button type="button" class="btn btn-outline-warning">Warning</button>
+    <button type="button" class="btn btn-outline-info">Info</button>
+    <button type="button" class="btn btn-outline-light">Light</button>
+    <button type="button" class="btn btn-outline-dark">Dark</button>
+    <p>era mi boton que es nuevo para vite</p>
+    <p><h1>este es mi proyecto</h1></p>
 @endsection
 
--- resources/views/layout/frontlayout.blade.php
+++ resources/views/layout/frontlayout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
+    @vite(['resourses/css/app.css','resourses/js/app.js'])
 </head>
 <body>
     @yield('content')
