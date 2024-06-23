597b46eeaac18802f3e946f944165bccae9a9cb1
wicho1965 <lualroru65>
2024-06-22 20:24:47 -0400

    2270672024 20:25

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Home')
 
 @section('content')
-    desde el home <<button type="button" class="btn btn-outline-primary">Primary</button>
+    desde el home <button type="button" class="btn btn-outline-primary">Primary</button>
     <button type="button" class="btn btn-outline-secondary">Secondary</button>
     <button type="button" class="btn btn-outline-success">Success</button>
     <button type="button" class="btn btn-outline-danger">Danger</button>
@@ -11,7 +11,7 @@
     <button type="button" class="btn btn-outline-info">Info</button>
     <button type="button" class="btn btn-outline-light">Light</button>
     <button type="button" class="btn btn-outline-dark">Dark</button>
-    <p>era mi boton que es nuevo para vite</p>
+    <p>este era mi boton que es nuevos para vite</p>
     <p><h1>este es mi proyecto</h1></p>
 @endsection
 
