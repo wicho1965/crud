02672d50f75e829cadeec3e432d56b9238276087
wicho1965 <lualroru65@gmail.com>
2024-11-05 21:44:01 -0300

    05/11/2024 21:43

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/categorias', function () {
+    return view('front.categorias');
+});
+
+
+Route::get('/single', function () {
+    return view('front.single');
+});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.home');
 });
